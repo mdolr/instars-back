f902dc880292263e7160e61d1f107a6b32c6262b
Maxime Dolores <19026937+mdolr@users.noreply.github.com>
2021-11-10 10:49:57 +0100

    feat: add a new endpoint
    + Delete .DS_Store

---
+++ src/main/java/com/tinyinsta/Users.java
@@ -0,0 +1,24 @@
+package com.tinyinsta;
+
+import com.google.api.server.spi.config.Api;
+import com.google.api.server.spi.config.ApiMethod;
+import com.google.api.server.spi.response.NotFoundException;
+import com.google.appengine.api.users.User;
+
+import java.util.ArrayList;
+
+import javax.inject.Named;
+
+@Api(
+    name = "users",
+    version = "v1",
+    scopes = {Constants.EMAIL_SCOPE},
+    clientIds = {Constants.WEB_CLIENT_ID}
+)
+public class Users {
+
+  @ApiMethod(name = "me", httpMethod = "get", path = "users/me")
+  public HelloGreeting get(@Named("id") Long id) throws NotFoundException {
+    return new HelloGreeting("goodbye world!");
+  }
+}
