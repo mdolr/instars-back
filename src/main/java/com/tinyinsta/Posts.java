66be0c0ebf8b1ef2fe4e1d8155880192d599c905
Nabil Oulbaz <50203530+NabilOulbaz@users.noreply.github.com>
2021-11-14 16:19:19 +0100

    feat: adding timeline endpoint.

--- src/main/java/com/tinyinsta/Posts.java
+++ src/main/java/com/tinyinsta/Posts.java
@@ -22,36 +22,27 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
-@Api(
-    name = "tinyinsta",
-    version = "v1",
-    scopes = {Constants.EMAIL_SCOPE},
-    clientIds = {Constants.WEB_CLIENT_ID}
-)
+@Api(name = "tinyinsta", version = "v1", scopes = { Constants.EMAIL_SCOPE }, clientIds = { Constants.WEB_CLIENT_ID })
 public class Posts {
 
     @ApiMethod(name = "getIndividual", httpMethod = "get", path = "posts/{id}")
-    public Entity getIndividual(
-        @Named("id") String id
-    ) throws EntityNotFoundException {
+    public Entity getIndividual(@Named("id") String id) throws EntityNotFoundException {
         DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
 
-        Entity e=new Entity("Post",id);
+        Entity e = new Entity("Post", id);
 
-        Entity e1=datastore.get(e.getKey());
+        Entity e1 = datastore.get(e.getKey());
 
         return e1;
     }
 
     @ApiMethod(name = "getAll", httpMethod = "get", path = "posts")
-    public List<Entity> getAll(
-        @Nullable @Named("owner") String owner
-    ) {
+    public List<Entity> getAll(@Nullable @Named("owner") String owner) {
         DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
 
         Query q = new Query("Post");
 
-        if(owner!=null) {
+        if (owner != null) {
             q.setFilter(new Query.FilterPredicate("owner", Query.FilterOperator.EQUAL, owner));
         }
 
@@ -62,9 +53,7 @@ public class Posts {
     }
 
     @ApiMethod(name = "requestprotectedURL", httpMethod = "get", path = "protectedURL")
-    public URL requestprotectedURL(
-        @Named("fileName") String fileName
-    ){
+    public URL requestprotectedURL(@Named("fileName") String fileName) {
         String projectId = "tinyinsta-web";
         String bucketName = "posts_test789";
 
@@ -75,7 +64,8 @@ public class Posts {
             e.printStackTrace();
         }
 
-        Storage storage = StorageOptions.newBuilder().setProjectId(projectId).setCredentials(credentials).build().getService();
+        Storage storage = StorageOptions.newBuilder().setProjectId(projectId).setCredentials(credentials).build()
+                .getService();
 
         // Define Resource
         BlobInfo blobInfo = BlobInfo.newBuilder(BlobId.of(bucketName, fileName)).build();
@@ -84,24 +74,14 @@ public class Posts {
         Map<String, String> extensionHeaders = new HashMap<>();
         extensionHeaders.put("Content-Type", "application/octet-stream");
 
-        URL url =
-                storage.signUrl(
-                        blobInfo,
-                        15,
-                        TimeUnit.MINUTES,
-                        Storage.SignUrlOption.httpMethod(HttpMethod.PUT),
-                        Storage.SignUrlOption.withExtHeaders(extensionHeaders),
-                        Storage.SignUrlOption.withV4Signature());
+        URL url = storage.signUrl(blobInfo, 15, TimeUnit.MINUTES, Storage.SignUrlOption.httpMethod(HttpMethod.PUT),
+                Storage.SignUrlOption.withExtHeaders(extensionHeaders), Storage.SignUrlOption.withV4Signature());
         return url;
     }
 
     @ApiMethod(name = "uploadPost", httpMethod = "post", path = "posts")
-    public Entity uploadPost(
-        @Named("url") String url,
-        @Named("owner") String owner,
-        @Named("title") String title,
-        @Named("description") String description
-    ) {
+    public Entity uploadPost(@Named("url") String url, @Named("owner") String owner, @Named("title") String title,
+            @Named("description") String description) {
         DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
         Transaction txn = datastore.beginTransaction();
         try {
@@ -112,11 +92,12 @@ public class Posts {
             String id = date_inverted_timestamp + "_" + owner;
 
             Entity e = new Entity("Post", id);
-            e.setProperty("url", url);//TODO: Change url to store url
+            e.setProperty("url", url);// TODO: Change url to store url
             e.setProperty("owner", owner);
             e.setProperty("title", title);
             e.setProperty("description", description);
             e.setProperty("date", date_formatted);
+            e.setProperty("createdAt", new Date());
             e.setProperty("likes", 0);
 
             datastore.put(e);
@@ -132,17 +113,15 @@ public class Posts {
     }
 
     @ApiMethod(name = "likePost", httpMethod = "put", path = "posts/{id}/like")
-    public Entity likePost(
-        @Named("id") String id
-    ) throws EntityNotFoundException {
+    public Entity likePost(@Named("id") String id) throws EntityNotFoundException {
         DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
         Transaction txn = datastore.beginTransaction();
         try {
-            Entity e=new Entity("Post",id);
+            Entity e = new Entity("Post", id);
 
-            Entity e1=datastore.get(e.getKey());
+            Entity e1 = datastore.get(e.getKey());
             long likes = (long) e1.getProperty("likes");
-            e1.setProperty("likes", likes+1);
+            e1.setProperty("likes", likes + 1);
             datastore.put(e1);
 
             txn.commit();
--- src/main/java/com/tinyinsta/Users.java
+++ src/main/java/com/tinyinsta/Users.java
@@ -3,7 +3,7 @@ package com.tinyinsta;
 import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
 import com.google.appengine.api.datastore.*;
-
+import com.google.appengine.api.datastore.Query.SortDirection;
 import com.google.auth.Credentials;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.storage.*;
@@ -36,20 +36,12 @@ import com.google.api.server.spi.response.ConflictException;
 import com.google.api.server.spi.response.InternalServerErrorException;
 import com.google.api.server.spi.response.ServiceUnavailableException;
 
-
-@Api(
-    name = "tinyinsta",
-    version = "v1",
-    scopes = {Constants.EMAIL_SCOPE},
-    clientIds = {Constants.WEB_CLIENT_ID}
-)
+@Api(name = "tinyinsta", version = "v1", scopes = { Constants.EMAIL_SCOPE }, clientIds = { Constants.WEB_CLIENT_ID })
 public class Users {
-  @ApiMethod(name = "me",
-              httpMethod = "get",
-              path = "users/me",
-              clientIds={"284772421623-8klntslq83finkqcgee2d3bi08rj7kt0.apps.googleusercontent.com"},
-              audiences={"284772421623-8klntslq83finkqcgee2d3bi08rj7kt0.apps.googleusercontent.com"},
-              scopes={"https://www.googleapis.com/auth/userinfo.email", "https://www.googleapis.com/auth/userinfo.profile"})
+  @ApiMethod(name = "me", httpMethod = "get", path = "users/me", clientIds = {
+      "284772421623-8klntslq83finkqcgee2d3bi08rj7kt0.apps.googleusercontent.com" }, audiences = {
+          "284772421623-8klntslq83finkqcgee2d3bi08rj7kt0.apps.googleusercontent.com" }, scopes = {
+              "https://www.googleapis.com/auth/userinfo.email", "https://www.googleapis.com/auth/userinfo.profile" })
   public Entity getSelf(User reqUser) throws UnauthorizedException, EntityNotFoundException {
     // If the user is not logged in : throw an error or redirect to the login page
     if (reqUser == null) {
@@ -76,9 +68,11 @@ public class Users {
       // - picture : the user picture
       // - createdAt : the date of creation
       // - updatedAt : the date of last update
-      // - followers : An UserFollowers entity which contains multiple lists of followers
-      // - following : An UserFollowings entity which contains multiple lists of followings
-      
+      // - followers : An UserFollowers entity which contains multiple lists of
+      // followers
+      // - following : An UserFollowings entity which contains multiple lists of
+      // followings
+
       // Create the user entity
       Entity newUser = new Entity("User", reqUser.getId());
 
@@ -110,13 +104,12 @@ public class Users {
     }
   }
 
-  @ApiMethod(name = "me",
-              httpMethod = "put",
-              path = "users/me",
-              clientIds={"284772421623-8klntslq83finkqcgee2d3bi08rj7kt0.apps.googleusercontent.com"},
-              audiences={"284772421623-8klntslq83finkqcgee2d3bi08rj7kt0.apps.googleusercontent.com"},
-              scopes={"https://www.googleapis.com/auth/userinfo.email", "https://www.googleapis.com/auth/userinfo.profile"})
-  public Entity updateSelf(User reqUser, Map<String, Object> reqBody) throws UnauthorizedException, EntityNotFoundException {
+  @ApiMethod(name = "me", httpMethod = "put", path = "users/me", clientIds = {
+      "284772421623-8klntslq83finkqcgee2d3bi08rj7kt0.apps.googleusercontent.com" }, audiences = {
+          "284772421623-8klntslq83finkqcgee2d3bi08rj7kt0.apps.googleusercontent.com" }, scopes = {
+              "https://www.googleapis.com/auth/userinfo.email", "https://www.googleapis.com/auth/userinfo.profile" })
+  public Entity updateSelf(User reqUser, Map<String, Object> reqBody)
+      throws UnauthorizedException, EntityNotFoundException {
     // If the user is not logged in : throw an error or redirect to the login page
     if (reqUser == null) {
       throw new UnauthorizedException("Authorization required");
@@ -129,37 +122,59 @@ public class Users {
     user.setProperty("updatedAt", new Date()); // Update the last updated date
 
     // Only allow update of certains variables
-    String[] mutableVariables = {"name", "handle", "email", "pictureURL"};
-    
+    String[] mutableVariables = { "name", "handle", "email", "pictureURL" };
+
     for (Map.Entry<String, Object> entry : reqBody.entrySet()) {
       // TODO: Make sure the key matches one of the mutable variables
       // String key = entry.getKey();
 
-      //if(Array.stream(mutableVariables).anyMatch(key::equals)) {
-        user.setProperty(entry.getKey(), entry.getValue()); // Update the property
-      //}
+      // if(Array.stream(mutableVariables).anyMatch(key::equals)) {
+      user.setProperty(entry.getKey(), entry.getValue()); // Update the property
+      // }
     }
-    
+
     // Update each parameter
 
     return user;
   }
 
-
   // A route to get an user by its handle
-  @ApiMethod(name = "handle",
-              httpMethod = "get",
-              path = "users/handle/{handle}")
+  @ApiMethod(name = "handle", httpMethod = "get", path = "users/handle/{handle}")
   public Entity getByHandle(@Named("handle") String handle) throws NotFoundException, EntityNotFoundException {
     // Query the datastore to get the user by its handle
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
     Query query = new Query("User").setFilter(new Query.FilterPredicate("handle", Query.FilterOperator.EQUAL, handle));
     Entity user = datastore.prepare(query).asSingleEntity();
 
-    if(user == null) {
+    if (user == null) {
       throw new NotFoundException("User not found");
     }
 
     return user;
   }
+
+  // A route to get timeline
+  @ApiMethod(name = "timeline", httpMethod = "get", path = "users/me/timeline", clientIds = {
+      "284772421623-8klntslq83finkqcgee2d3bi08rj7kt0.apps.googleusercontent.com" }, audiences = {
+          "284772421623-8klntslq83finkqcgee2d3bi08rj7kt0.apps.googleusercontent.com" }, scopes = {
+              "https://www.googleapis.com/auth/userinfo.email", "https://www.googleapis.com/auth/userinfo.profile" })
+  public List<Entity> getTimeline(User reqUser) throws UnauthorizedException, EntityNotFoundException {
+    // If the user is not logged in : throw an error or redirect to the login page
+    if (reqUser == null) {
+      throw new UnauthorizedException("Authorization required");
+    }
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    // Query query = new Query("Post").setFilter(new Query.FilterPredicate("owner",
+    // Query.FilterOperator.EQUAL, "UserFollowings")).addSort("createdAt",
+    // Query.SortDirection.DESCENDING)
+    Entity userFollowings = datastore.get(KeyFactory.createKey("UserFollowings", reqUser.getId()));
+    ArrayList<String> followings = (ArrayList<String>) userFollowings.getProperty("followingsBatch-1");
+    Query query = new Query("Post")
+        .setFilter(new Query.FilterPredicate("owner", Query.FilterOperator.EQUAL, followings))
+        .addSort("createdAt", Query.SortDirection.DESCENDING);
+    QueryResultList<Entity> result = datastore.prepare(query).asQueryResultList(FetchOptions.Builder.withLimit(10));
+
+    return result;
+  }
+
 }
