3e302e10e3a843c6b1d5ea027cc1fef276244cf8
jjbes <rikuleixjun@gmail.com>
2021-11-17 14:44:28 +0100

    feat: likes count

---
+++ src/main/java/com/tinyinsta/common/AvailableBatches.java
@@ -0,0 +1,57 @@
+package com.tinyinsta.common;
+
+import com.google.appengine.api.datastore.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AvailableBatches {
+    private String kind;
+    private Key ancestorKey;
+    private List<Entity> availableBatches;
+    private int number;
+
+    public AvailableBatches(String kind, Key ancestorKey) {
+        this.kind = kind;
+        this.ancestorKey = ancestorKey;
+
+        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+
+        Query getBatchesQuery = new Query(this.kind)
+                .setAncestor(this.ancestorKey)
+                .setFilter(new Query.FilterPredicate("size", Query.FilterOperator.LESS_THAN, Constants.MAX_BATCH_SIZE));
+
+        this.availableBatches =
+                datastore.prepare(getBatchesQuery).asQueryResultList(FetchOptions.Builder.withDefaults());
+
+        this.number = this.availableBatches.size();
+    }
+
+    public Entity getRandom() {
+        Entity availableBatch;
+        ArrayList<String> batch = new ArrayList<String>(); // In case we get an empty batch we need to declare it
+
+        int randomBatch = new RandomGenerator().get(0, batch.size());
+
+        if(this.availableBatches.size() > 0) {
+            availableBatch = this.availableBatches.get(randomBatch);
+        } else {
+            availableBatch = new Entity(this.kind, this.ancestorKey);
+        }
+
+        return availableBatch;
+    }
+
+    public int getAvailableBatchesLikeCount(){
+        int count = 0;
+        for (Entity batch : this.availableBatches){
+            count += (int) batch.getProperty("size");
+        }
+
+        return count;
+    }
+
+    public int getNumber() {
+        return this.number;
+    }
+}
