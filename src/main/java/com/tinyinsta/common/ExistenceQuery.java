b5becf9ccb5e836ceaf2aed8b66c2015ccf7e0c2
Maxime Dolores <19026937+mdolr@users.noreply.github.com>
2021-11-26 22:05:20 +0100

    fix: set composite filter on existence query

--- src/main/java/com/tinyinsta/common/ExistenceQuery.java
+++ src/main/java/com/tinyinsta/common/ExistenceQuery.java
@@ -2,11 +2,11 @@ package com.tinyinsta.common;
 
 import com.google.api.server.spi.response.ConflictException;
 import com.google.appengine.api.datastore.*;
+import com.google.appengine.api.datastore.Query.*;
 
 public class ExistenceQuery {
     public boolean check(String kind, Key ancestorKey, String searchedValue) throws ConflictException {
         DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-
 
         Query existenceQuery;
 
@@ -15,13 +15,17 @@ public class ExistenceQuery {
                     .setAncestor(ancestorKey)
                     .setFilter(new Query.FilterPredicate("batch", Query.FilterOperator.EQUAL, searchedValue));
         } else {
+            Filter parentFilter = new FilterPredicate("parentId", FilterOperator.EQUAL, ancestorKey.getName());
+            Filter batchFilter = new FilterPredicate("batch", FilterOperator.EQUAL, searchedValue);
+            CompositeFilter filter = CompositeFilterOperator.and(parentFilter, batchFilter);
+
             existenceQuery = new Query(kind)
-                    .setFilter(new Query.FilterPredicate("parentId", Query.FilterOperator.EQUAL, ancestorKey.getName()))
-                    .setFilter(new Query.FilterPredicate("batch", Query.FilterOperator.EQUAL, searchedValue)); 
+                    .setFilter(filter)
+                    .setKeysOnly(); 
         }
         
         QueryResultList<Entity> existenceResults = datastore.prepare(existenceQuery).asQueryResultList(FetchOptions.Builder.withLimit(1));
-
+        
         // If we find a result then it means that the user is already following the target
         return existenceResults.size() > 0;
     }
